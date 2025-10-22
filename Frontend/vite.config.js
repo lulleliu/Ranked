6509b396d96099a84ba3039c44d93bdf23daa612
Lucas <lucasliu@live.se>
2025-10-23 01:43:31 +0200

    added render hosting support

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:8000", // Forward /api calls to backend
+export default ({ mode }) => {
+  // Load environment variables based on mode (development, production)
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": env.VITE_BACKEND_URL, // Use env variable here
+      },
     },
-  },
-});
+  });
+};
